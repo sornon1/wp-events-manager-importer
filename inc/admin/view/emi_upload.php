7a6753102184d0b433a05fb2be9e9c660b6f30f1
Soren Rehn <sornon1@gmail.com>
2017-04-23 22:18:22 +0100

    Change wording of geocoding option and default it to checked.

--- inc/admin/view/emi_upload.php
+++ inc/admin/view/emi_upload.php
@@ -1,14 +1,14 @@
 <div class="emi_upload_wrap">
 	<h1>Upload</h1>
 	<form id="upload_form" method="post" enctype="multipart/form-data" action="<?php echo $this->createFormAction($this->action_importviewer); ?>">
-			
+
 				<div class="emi_droparea">
 					<p>(.xlsx <?php _e("or","emi"); ?> .csv)</p>
 					<input type="file" name="emi_file" /><br/>
 					<p><?php _e("or","emi"); ?>
 					<p><?php _e("Drop your file here","emi"); ?></p>
 				</div>
-			<label><input type="checkbox" name="geocoding" value="true" /><?php _e("Enable geocoding to retrieve location informations. (May take a while)", "emi"); ?></label>
+				<label><input type="checkbox" name="geocoding" value="true" checked/><?php _e("Add address information from Google Maps.", "emi"); ?></label>
 			<input class="button emi_upload" type="submit" value="<?php _e('Upload and Preview', 'emi'); ?>" />
 	</form>
 </div>
